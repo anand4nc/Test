8954f8163975b298592bdc4526788357c6776266
anand4nc <59688007+anand4nc@users.noreply.github.com>
2020-12-02 10:11:24 +0530

    Update filterTerm.spec.js

--- __tests__/filterTerm.spec.js
+++ __tests__/filterTerm.spec.js
@@ -5,7 +5,7 @@ describe("Filter function", () => {
     const input = [
       { id: 1, url: "https://www.url1.dev" },
       { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "https://www.link3.dev" }
+      { id: 3, url: "https://www.url3.dev" }
     ];
 
     const output = [{ id: 3, url: "https://www.link3.dev" }];
